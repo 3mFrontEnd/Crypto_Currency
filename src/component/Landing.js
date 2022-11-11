022133b9412c345174d7cc2e526f4d88d817d102
Mohammad Mohammadi <3mfrontend@gmail.com>
2022-11-10 18:45:16 -0800

    filter for search bar added to codes

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect} from 'react';
 
 // components
 import Loader from './Loader';
+import Coin from './Coin';
 
 // API
 import { getCoin } from '../services/api';
@@ -9,6 +10,7 @@ import { getCoin } from '../services/api';
 const Landing = () => {
 
     const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState([]);
 
     useEffect(() => {
         const fetchAPI = async () =>{
@@ -20,14 +22,28 @@ const Landing = () => {
         fetchAPI()
     },[]);
 
+    const searchHandler = event => {
+        setSearch(event.target.value)
+    }
+
+    const searchCoins = coins.filter(coin => coin.name.toString().toLowerCase().includes(search.toString().toLowerCase()))
+
     return (
         <>
-            <input type="text" placeholder='Search' />
+            <input type="text" placeholder='Search' value={search} onChange={searchHandler} />
             {
                 coins.length ?
                     <div>
                         {
-                            coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+                            searchCoins.map(coin => <Coin 
+                                    key={coin.id}
+                                    name = {coin.name}
+                                    image = {coin.image}
+                                    symbol = {coin.symbol}
+                                    price = {coin.current_price}
+                                    marketCap = {coin.market_cap}
+                                    priceChange = {coin.price_change_percentage_24h}
+                                />)
                         }
                     </div> :
                     <Loader />
