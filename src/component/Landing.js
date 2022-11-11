7f88e3e95f31790a70ef4c5f327f5a40e703ca8f
Mohammad Mohammadi <3mfrontend@gmail.com>
2022-11-10 18:02:19 -0800

    some condition are added to landing component and also loader component was add for showing loading until API catch form backend

--- src/component/Landing.js
+++ src/component/Landing.js
@@ -1,23 +1,38 @@
 import React,{useState, useEffect} from 'react';
 
+// components
+import Loader from './Loader';
+
 // API
 import { getCoin } from '../services/api';
 
 const Landing = () => {
 
+    const [coins, setCoins] = useState([]);
+
     useEffect(() => {
-        const feachAPI = async () =>{
+        const fetchAPI = async () =>{
             const data = await getCoin();
             console.log(data)
+            setCoins(data)
         }
 
-        feachAPI()
+        fetchAPI()
     },[]);
 
     return (
-        <div>
-            Landing
-        </div>
+        <>
+            <input type="text" placeholder='Search' />
+            {
+                coins.length ?
+                    <div>
+                        {
+                            coins.map(coin => <p key={coin.id}>{coin.name}</p>)
+                        }
+                    </div> :
+                    <Loader />
+            }
+        </>
     );
 };
 
